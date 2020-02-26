2f93348b3ee46120f8d4c25080088d9b0daa3a32
Ludwig Havasi <ludwig.havasi@gmail.com>
2020-02-26 11:48:08 +0100

    Started GetMapping in QuizController

--- src/main/java/org/hhjml/miniprojekt2/CheeseQuiz.java
+++ src/main/java/org/hhjml/miniprojekt2/CheeseQuiz.java
@@ -4,18 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CheeseQuiz {
-    String result1 = "Parmesan";
-    String result2 = "Cheddar";
-    String result3 = "Gouda";
-    String result4 = "Pepper jack";
+    private String result1 = "Parmesan";
+    private String result2 = "Cheddar";
+    private String result3 = "Gouda";
+    private String result4 = "Pepper jack";
 
-    List<Question> questionArray = new ArrayList<>();
+    private List<Question> questionArray = new ArrayList<>();
 
     public CheeseQuiz() {
-        questionArray.add(new Question("Favourite color?", "Blue", "Red", "Green", "Cheesy yellow"));
-        questionArray.add(new Question("Favourite food?", "Cheese, duh!", "MEAT!", "Pasta!", "Oysters"));
-        questionArray.add(new Question("Favourite animal?", "Cat", "Dog", "Giraffe", "Pink fairy armadillo"));
-        questionArray.add(new Question("Favourite sci-fi franchise?", "Doctor who", "Star wars", "Star trek", "Battlestar Galactica"));
+        getQuestionArray().add(new Question("Favourite color?", "Blue", "Red", "Green", "Cheesy yellow"));
+        getQuestionArray().add(new Question("Favourite food?", "Cheese, duh!", "MEAT!", "Pasta!", "Oysters"));
+        getQuestionArray().add(new Question("Favourite animal?", "Cat", "Dog", "Giraffe", "Pink fairy armadillo"));
+        getQuestionArray().add(new Question("Favourite sci-fi franchise?", "Doctor who", "Star wars", "Star trek", "Battlestar Galactica"));
     }
 
+    public String getResult1() {
+        return result1;
+    }
+
+    public String getResult2() {
+        return result2;
+    }
+
+    public String getResult3() {
+        return result3;
+    }
+
+    public String getResult4() {
+        return result4;
+    }
+
+    public List<Question> getQuestionArray() {
+        return questionArray;
+    }
 }
--- src/main/java/org/hhjml/miniprojekt2/QuizController.java
+++ src/main/java/org/hhjml/miniprojekt2/QuizController.java
@@ -1,8 +1,11 @@
 package org.hhjml.miniprojekt2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class QuizController {
+
+    @GetMapping("/cheesequiz")
 
 }
