0a95ae5d199313e6d99c65751c5267b4536c3918
Metsis <hanna.metsis@gmail.com>
2020-02-26 10:32:27 +0100

    init

---
+++ src/main/java/org/hhjml/miniprojekt2/Miniprojekt2Application.java
@@ -0,0 +1,13 @@
+package org.hhjml.miniprojekt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miniprojekt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Miniprojekt2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/hhjml/miniprojekt2/Miniprojekt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hhjml.miniprojekt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miniprojekt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
