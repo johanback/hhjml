08cea28c3cdb08be9320e32b160dc6a84cffc737
Johan Back <johan.g.back@gmail.com>
2020-02-27 14:01:46 +0100

    Created result class.

---
+++ src/main/java/org/hhjml/miniprojekt2/Result.java
@@ -0,0 +1,20 @@
+package org.hhjml.miniprojekt2;
+
+public class Result {
+
+    private String result;
+    private String description;
+    private String imgUrl;
+
+    public String getResult() {
+        return result;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
