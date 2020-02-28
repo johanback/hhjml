0c505ea426aaa7dbafaf42b0142922107a920916
Metin Akbal <metin_aa@hotmail.com>
2020-02-28 09:38:24 +0100

    Marvel quiz

---
+++ src/main/java/org/hhjml/miniprojekt2/MarvelQuiz.java
@@ -0,0 +1,49 @@
+package org.hhjml.miniprojekt2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarvelQuiz implements Quiz {
+
+    private String quizName = "Marvel";
+
+    private Result result1 = new Result("Iron-man", "Genius. Billionaire. Playboy. Philanthropist.", "parmesan.jpg");
+    private Result result2 = new Result("Hulk", "Dr. Bruce Banner lives a life caught between the soft-spoken scientist he’s always been and the uncontrollable green monster powered by his rage.", "gouda.jpg");
+    private Result result3 = new Result("Captian America", "Recipient of the Super-Soldier serum, World War II hero Steve Rogers fights for American ideals as one of the world’s mightiest heroes and the leader of the Avengers.", "cheddar.jpg");
+    private Result result4 = new Result("Thor", "The son of Odin uses his mighty abilities as the God of Thunder to protect his home Asgard and planet Earth alike.", "pepperjack.jpg");
+
+    private List<Question> questionArray = new ArrayList<>();
+
+    public MarvelQuiz() {
+        getQuestionArray().add(new Question("What kind of pizza do you like?", "Oxfile pizza", "Kebabpizza", "Margarita", "Calzone"));
+        getQuestionArray().add(new Question("Favorite Tv-show when you were young?", "Fresh prince in bel air", "Magnum PI", "Full house", "Golden girls"));
+        getQuestionArray().add(new Question("Your choice of liquid?", "Whisky", "Milk", "Manhattan", "Mead"));
+        getQuestionArray().add(new Question("Choose the destination you would like to travel to?", "The Moon", "Cave", "City", "Alps"));
+        getQuestionArray().add(new Question("Choose a president?", "Vladimir Putin", "Kim jong un", "George W Bush", "Donald trump"));
+    }
+
+    public Result getResult(Character resultChar) {
+        switch (resultChar) {
+            case 'A':
+                return result1;
+            case 'B':
+                return result2;
+            case 'C':
+                return result3;
+            case 'D':
+                return result4;
+            default:
+                return null;
+        }
+    }
+
+    public List<Question> getQuestionArray() {
+        return questionArray;
+    }
+
+
+    public String getQuizName() {
+        return quizName;
+    }
+
+}
--- src/main/java/org/hhjml/miniprojekt2/QuizRepository.java
+++ src/main/java/org/hhjml/miniprojekt2/QuizRepository.java
@@ -12,11 +12,13 @@ public class QuizRepository {
 
     CheeseQuiz cheeseQuiz = new CheeseQuiz();
     ShoeQuiz shoeQuiz = new ShoeQuiz();
+    MarvelQuiz marvelQuiz = new MarvelQuiz();
     CarBrandQuiz carBrandQuiz = new CarBrandQuiz();
 
     public QuizRepository() {
         quizDB.put(cheeseQuiz.getQuizName(), cheeseQuiz);
         quizDB.put(shoeQuiz.getQuizName(), shoeQuiz);
+        quizDB.put(marvelQuiz.getQuizName(), marvelQuiz);
         quizDB.put(carBrandQuiz.getQuizName(), carBrandQuiz);
     }
 
