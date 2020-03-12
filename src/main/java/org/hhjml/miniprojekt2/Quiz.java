3de313cbd489d6ede18b0b0b898bea9cfa3af4a5
Ludwig Havasi <ludwig.havasi@gmail.com>
2020-03-12 10:09:59 +0100

    Correct dependencies and started annotations in classes.

--- src/main/java/org/hhjml/miniprojekt2/Quiz.java
+++ src/main/java/org/hhjml/miniprojekt2/Quiz.java
@@ -1,11 +1,15 @@
 package org.hhjml.miniprojekt2;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.List;
 
 @Entity
 public class Quiz {
     @Id
-    @GenratedValue(strategy = generationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private List<Result> resultList;
