f478596108cf6934b1df68e34f04c07b8b0cd13d
Ludwig Havasi <ludwig.havasi@gmail.com>
2020-03-12 16:57:30 +0100

    Skapade "skalet" för metoderna som ska returnera en färdig quiz.

--- src/main/java/org/hhjml/miniprojekt2/QuizRepository.java
+++ src/main/java/org/hhjml/miniprojekt2/QuizRepository.java
@@ -3,11 +3,13 @@ package org.hhjml.miniprojekt2;
 import org.hhjml.quizzes.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.method.annotation.AbstractWebArgumentResolverAdapter;
 
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @Repository
 public class QuizRepository {
@@ -21,6 +23,7 @@ public class QuizRepository {
 
         /*1. Quiz skapas, får sitt namn
         2. Metod som skapar en ArrayList av frågor som sen sätts till quizens sån arraylist.
+            //quiz.setQuestionsArray(getQuestions(quiz))
         3. Metod som sätter resultaten
         4. Returnerar färdigt quiz.
          */
@@ -43,6 +46,23 @@ public class QuizRepository {
         }
 
         return quiz;
+    }
+
+    private List<Question> getQuestions (Quiz quiz) {
+        List<Question> qarray = new ArrayList<>();
+        //Gör en query som hämtar varje question baserat på quizens id.
+        //Gör en metod som tar question och stoppar in alla svar.
+
+        getAnswers();
+
+        return qarray;
+
+    }
+
+    private List<Answer> getAnswers (Question question) {
+        List<Answer> answerList = new ArrayList<>();
+        //Gör en question som hämtar varje answer baserat på question id.
+        return answerList;
     }
 
     public ArrayList<String> getQuizNames () {
