6682e1e36aa71254313fe603f87140de3eb97788
Ludwig Havasi <ludwig.havasi@gmail.com>
2020-03-13 14:05:44 +0100

    Reintroduced the Random Quiz on the front page

--- src/main/java/org/hhjml/miniprojekt2/QuizService.java
+++ src/main/java/org/hhjml/miniprojekt2/QuizService.java
@@ -59,13 +59,13 @@ public class QuizService {
         return mostAnswered;
     }
 
-//    public String getRandomQuiz(){
-//        Random r = new Random();
-//        int randomNumber = r.nextInt(getQuizNames().size() - 1);
-//        String randomName = getQuizNames().get(randomNumber);
-//        return randomName;
-//
-//    }
+    public String getRandomQuiz(){
+        Random r = new Random();
+        int randomNumber = r.nextInt(getQuizNames().size() - 1);
+        String randomName = getQuizNames().get(randomNumber);
+        return randomName;
+
+    }
 
 
 
