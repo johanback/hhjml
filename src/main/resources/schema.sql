b700f62ef497ce61076aa917712a9663f9029925
Metsis <hanna.metsis@gmail.com>
2020-03-13 10:04:50 +0100

    schema.sql and data.sql created

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Quiz(
+	ID BIGINT AUTO_INCREMENT PRIMARY KEY,
+	Name nvarchar(50) NULL
+);
+
+CREATE TABLE Question(
+	ID BIGINT AUTO_INCREMENT PRIMARY KEY,
+	Question nvarchar(100) NULL,
+	QuizID BIGINT NOT NULL
+);
+
+CREATE TABLE Answer(
+	ID BIGINT AUTO_INCREMENT PRIMARY KEY,
+	Answer nvarchar(50) NULL,
+	ResultChar nchar(1) NULL,
+	QuestionID BIGINT NULL
+);
+
+CREATE TABLE Result(
+	ID BIGINT AUTO_INCREMENT PRIMARY KEY,
+	Result nvarchar(50) NULL,
+	Description nvarchar(MAX) NULL,
+	ImgUrl nvarchar(50) NULL,
+	ResultChar nchar(1) NULL,
+	QuizID BIGINT NULL
+);
+
+ALTER TABLE Question ADD FOREIGN KEY (QuizID) REFERENCES Quiz(ID);
+ALTER TABLE Answer ADD FOREIGN KEY (QuestionID) REFERENCES Question(ID);
+ALTER TABLE Result ADD FOREIGN KEY (QuizID) REFERENCES Quiz(ID);
+
